65bf89767644e843eef7821f8e02ac64cb307be7
Muhammadrajab <muhammadr0jabxujaev@gmail.com>
2023-04-30 16:05:45 +0500

    feat: add main keyboards div

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ let divMain = document.createElement("div");
 let divTextArea = document.createElement("div");
 let textArea = document.createElement("textarea");
 let keyboards = document.createElement("div");
-let first_column = document.createElement("div");
+let first_row = document.createElement("div");
 //Add Class
 divMain.classList.add("main");
 divTextArea.classList.add("textarea");
 textArea.classList.add("textarea-content");
 keyboards.classList.add("conttainer-keyboard");
-first_column.classList.add("fist_column");
+first_row.classList.add("fist_row");
 
 // Appends
 divTextArea.appendChild(textArea);
 divMain.appendChild(divTextArea);
-keyboards.appendChild(first_column);
+keyboards.appendChild(first_row);
+divMain.appendChild(keyboards);
 document.body.appendChild(divMain);
 // Ohter thing
 textArea.rows = 10;
