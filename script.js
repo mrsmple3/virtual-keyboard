444c0e9a0b34107e554e3bd47f26460a4123f205
Muhammadrajab <muhammadr0jabxujaev@gmail.com>
2023-04-30 18:52:21 +0500

    init: add buttons

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ let divMain = document.createElement("div");
 let divTextArea = document.createElement("div");
 let textArea = document.createElement("textarea");
 let keyboards = document.createElement("div");
-let first_row = document.createElement("div");
+let firstRow = document.createElement("div");
+let firstRowContent = new Array(14);
+for (let i = 0; i < 13; i++) {
+  firstRowContent[i] = document.createElement("button");
+  firstRowContent[i].classList.add("btn_common");
+  firstRow.appendChild(firstRowContent[i]);
+}
 //Add Class
 divMain.classList.add("main");
 divTextArea.classList.add("textarea");
 textArea.classList.add("textarea-content");
 keyboards.classList.add("conttainer-keyboard");
-first_row.classList.add("fist_row");
+firstRow.classList.add("fist_row");
 
 // Appends
 divTextArea.appendChild(textArea);
 divMain.appendChild(divTextArea);
-keyboards.appendChild(first_row);
+keyboards.appendChild(firstRow);
 divMain.appendChild(keyboards);
 document.body.appendChild(divMain);
 // Ohter thing
