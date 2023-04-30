93704744e30ac8d33e4152377be4b1e9338f9824
Muhammadrajab <muhammadr0jabxujaev@gmail.com>
2023-04-30 15:50:36 +0500

    feat: add commits for script.js file

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+//Create a new tegs
 let divMain = document.createElement("div");
 let divTextArea = document.createElement("div");
 let textArea = document.createElement("textarea");
 let keyboards = document.createElement("div");
 let first_column = document.createElement("div");
-
+//Add Class
 divMain.classList.add("main");
 divTextArea.classList.add("textarea");
 textArea.classList.add("textarea-content");
 keyboards.classList.add("conttainer-keyboard");
 first_column.classList.add("fist_column");
-textArea.rows = 10;
+
+// Appends
 divTextArea.appendChild(textArea);
 divMain.appendChild(divTextArea);
 keyboards.appendChild(first_column);
 document.body.appendChild(divMain);
+// Ohter thing
+textArea.rows = 10;
